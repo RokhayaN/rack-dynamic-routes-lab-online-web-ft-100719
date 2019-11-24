6dae1058d449e68dbc3cdf3ec007dd7fe59d1c32
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 21:42:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,9 @@ class Application
       if req.path == "/items/<ITEM NAME>"
         resp.write "#{item.price}./n"
       else 
-        resp.write "Unfortunatly,we do not carry this!"
-        resp.status 404
+        resp.write "Route not found!"
+        resp.status = 404
+      end 
+      resp.finish
     
  
