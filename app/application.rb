d44de78625392a53e8fd4a883b27dc49efafaca3
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-24 21:41:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,9 @@ class Application
     resp = Rack::Response.new  
     req = Rack::Response.new(env) 
       if req.path == "/items/<ITEM NAME>"
-        
+        resp.write "#{item.price}./n"
+      else 
+        resp.write "Unfortunatly,we do not carry this!"
+        resp.status 404
     
  
